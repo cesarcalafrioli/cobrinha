142a224545ae8ca33621c457dcbd177af75f00a5
César Calafrioli <46635135+cesarcalafrioli@users.noreply.github.com>
2021-06-16 19:10:31 -0300

    Atualizações

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Projeto de jogo da cobrinha.
 ##### 16/06/2021 #####
 
 - Definição dos movimentos da cobrinha
+- Criação da comida da cobra
 
 ##### 15/06/2021 #####
 
--- script.js
+++ script.js
@@ -9,6 +9,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 //////////////////////////
 ///////// FUNÇÕES ////////
@@ -26,10 +30,33 @@ function criarCobrinha(){
     }
 }
 
+// Cria a comida
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update ( event ) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 // Dar vida à cobrinha
 function iniciarJogo(){
+
+    // 
+    if ( snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if ( snake[0].x < 0 && direction == "left" ) snake[0].x = 16 * box;
+    if ( snake[0].y > 15 * box && direction == "up" ) snake[0].y = 0;
+    if ( snake[0].y < 0 && direction == "down" ) snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     // Posição inicial
     let snakeX = snake[0].x; 
