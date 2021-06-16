41923bf4b751eca48768298ecbf05d648c2f3a06
César Calafrioli <46635135+cesarcalafrioli@users.noreply.github.com>
2021-06-16 17:53:10 -0300

    Atualização

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Projeto de jogo da cobrinha.
 
 ### Atualizações ###
 
+##### 16/06/2021 #####
+
+- Definição dos movimentos da cobrinha
+
 ##### 15/06/2021 #####
 
 - Criação do projeto
--- script.js
+++ script.js
@@ -1,10 +1,57 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 
+}
+
+let direction = "right";
+
+//////////////////////////
+///////// FUNÇÕES ////////
+// Criar background
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+// Criar a cobrinha
+function criarCobrinha(){
+    for( i = 0 ; i < snake.length; i++ ) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+// Dar vida à cobrinha
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    // Posição inicial
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    // Movimento conforme as direcionais
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+//// FIM DAS FUNÇÕES //////
+///////////////////////////
+
+let jogo = setInterval(iniciarJogo, 100);
