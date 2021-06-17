a3caa129978a1103d9e433dea46627bd7c86c008
César Calafrioli <46635135+cesarcalafrioli@users.noreply.github.com>
2021-06-17 17:54:39 -0300

    Conclusão do projeto

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Projeto de jogo da cobrinha.
 
 ### Atualizações ###
 
+##### 17/06/2021 #####
+
+- Conclusão do projeto.
+
 ##### 16/06/2021 #####
 
 - Definição dos movimentos da cobrinha
--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/* script.js
+Autor = César Calafrioli
+Data = 14/06/2021
+Última modificação = 17/06/2021
+*/
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -48,7 +53,15 @@ function update ( event ) {
 // Dar vida à cobrinha
 function iniciarJogo(){
 
-    // 
+    // Se a cabeça da cobra se chocar com o corpo, o jogo para
+    for ( i = 1 ; i < snake.length; i++ ){
+        if ( snake[0].x == snake[i].x && snake[0].y == snake[i].y ){
+            clearInterval(jogo);
+            alert('Game Over!');
+        }
+    }
+
+    // Reposiciona a cobrinha para o lado oposto
     if ( snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if ( snake[0].x < 0 && direction == "left" ) snake[0].x = 16 * box;
     if ( snake[0].y > 15 * box && direction == "up" ) snake[0].y = 0;
@@ -68,7 +81,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY += box;
     if(direction == "down") snakeY -= box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+       food.x = Math.floor(Math.random() * 15 + 1) * box;
+       food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
@@ -81,4 +100,4 @@ function iniciarJogo(){
 //// FIM DAS FUNÇÕES //////
 ///////////////////////////
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 200);
